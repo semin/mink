bcfb6fe4fec65d27926afbd23ee7950e114e9491
Semin <semin@mordred.bioc.cam.ac.uk>
2010-04-08 13:15:29 +0100

    Fixed gems dependancy issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -23,9 +23,9 @@ Rails::Initializer.run do |config|
   config.gem 'narray'
   config.gem 'uuid'
   config.gem 'configatron'
-  config.gem 'freelancing-god-thinking-sphinx', :lib => 'thinking_sphinx', :source => 'http://gems.github.com'
-  config.gem 'alexvollmer-daemon-spawn', :lib => 'daemon-spawn', :source => 'http://gems.github.com'
-  config.gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com'
+  config.gem 'thinking-sphinx', :lib => 'thinking_sphinx'
+  config.gem 'daemon-spawn'
+  config.gem 'paperclip'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
