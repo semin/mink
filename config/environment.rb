c72cf77e5585db1eb8633c70d474c195b58bc9d7
Semin Lee <seminlee@gmail.com>
2010-06-02 09:58:14 +0100

    Changed library name, 'daemon-spawn' to 'daemon_spawn' in ./script/delayed_job

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,11 +20,11 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'narray'
   config.gem 'uuid'
+  config.gem 'narray'
   config.gem 'paperclip'
   config.gem 'configatron'
-  config.gem 'daemon-spawn'
+  config.gem 'daemon-spawn', :lib => 'daemon_spwan'
   config.gem 'ar-extensions'
   config.gem 'parallel-forkmanager', :lib => 'forkmanager'
   config.gem 'thinking-sphinx', :lib => 'thinking_sphinx', :version => '1.3.16'
